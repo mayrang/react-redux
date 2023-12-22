f9d372bb94098572b0e889e10a72b73ab460e16a
mayrang <pkss0626@naver.com>
2023-12-23 04:03:20 +0900

    first commit

---
+++ hello/simple1.js
@@ -0,0 +1,20 @@
+function LikedButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? "좋아요 취소" : "좋아요";
+
+  return React.createElement("button", { onClick: () => setLiked(!liked) }, text);
+}
+
+const domContainer = document.getElementById("root");
+
+ReactDOM.render(
+  React.createElement(
+    "div",
+    null,
+    React.createElement(LikedButton),
+    React.createElement(LikedButton),
+    React.createElement(LikedButton)
+  ),
+
+  domContainer
+);
